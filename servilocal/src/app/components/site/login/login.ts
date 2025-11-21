418b465b645cd0298c7adf08ddf0eee2dccbc3bb
Alex <ja181197@gmail.com>
2025-11-20 21:05:50 -0600

    creacion de login con tokens

--- servilocal/src/app/components/site/login/login.ts
+++ servilocal/src/app/components/site/login/login.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import {Router, RouterModule} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  tipo: 'usuario' | 'trabajador' = 'usuario';
+  form: any = {};
 
+  constructor(private authService: AuthService, private router: Router){}
+
+  iniciarSesion() {
+    const datos = { ...this.form, tipo: this.tipo};
+    this.authService.login(datos).subscribe({
+      next:(res:any) => {
+        alert(`Bienvenido ${this.tipo}`);
+        this.router.navigate(['/']);
+      },
+      error: err => alert(err.error.error || 'Error al iniciar sesion')
+    });
+  }
 }
--- servilocal/src/app/layouts/menu/menu.ts
+++ servilocal/src/app/layouts/menu/menu.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './menu.html',
   styleUrl: './menu.css'
 })
 export class Menu {
+  nombreUsuario: string | null = null;
+
+  constructor(private router: Router) {}
+
+  isLoggedIn(): boolean {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.nombreUsuario = localStorage.getItem('nombreUsuario');
+      return true;
+    }
+    return false;
+  }
+
+  irLogin() {
+    this.router.navigate(['site/login']);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('nombreUsuario');
+    this.router.navigate(['/']);
+  }
 
 }
